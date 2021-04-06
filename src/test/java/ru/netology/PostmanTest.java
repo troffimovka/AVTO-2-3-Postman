4cd5b17f495be823b3cc5e0a59e3264d44d0309b
troffimovka <troffimovka@gmail.com>
2021-04-06 22:54:08 +0300

    post

---
+++ src/test/java/ru/netology/PostmanTest.java
@@ -0,0 +1,23 @@
+package ru.netology;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+
+public class PostmanTest {
+    @Test
+    void shouldReturnPostRequest() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+                .contentType(ContentType.JSON);
+    }
+}
